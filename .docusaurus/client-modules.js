ebc201828dff275dfdd12f23f802d9d3fb240355
kehuo <hkhuoke@hotmail.com>
2020-09-27 14:47:45 +0800

    remove .docusaurus

--- .docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("/Users/hk/dev/website-frontend/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/hk/dev/website-frontend/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/hk/dev/website-frontend/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/hk/dev/website-frontend/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/hk/dev/website-frontend/node_modules/@docusaurus/theme-classic/src/prism-include-languages"),
-  require("/Users/hk/dev/website-frontend/src/css/custom.css"),
-];
